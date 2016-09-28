1b1cb73f9d492134b7d218fca4e42a6409712c80
zentalent <zentalent@163.com>
2016-09-28 11:33:18 +0800

    push some files

--- app/src/main/java/com/android/extremez/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/android/extremez/coolweather/activity/MainActivity.java
@@ -1,9 +1,12 @@
-package com.android.extremez.coolweather;
+package com.android.extremez.coolweather.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.extremez.coolweather.R;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
